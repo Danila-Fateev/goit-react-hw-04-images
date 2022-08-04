edb56ede731ebacee0501242e582cca36afed0c2
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-04 16:00:56 +0200

    1

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,27 @@
+import '../styles.css';
+import PropTypes from 'prop-types';
+
+export default function Searchbar({ onSubmit }) {
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={onSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
+
+        <input
+          name="formInput"
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
