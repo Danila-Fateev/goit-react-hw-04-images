a2e45bb9df6dbb105b096abccd56fa19d815395f
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-05 16:18:03 +0200

    finale

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import Loader from './Loader/Loader';
 
 export class App extends Component {
   state = {
-    page: 1,
+    page: null,
     items: [],
     isLoading: false,
     searchWord: '',
@@ -17,82 +17,70 @@ export class App extends Component {
     total: 0,
   };
 
-  submitForm = async e => {
-    e.preventDefault();
+  async fetchData(searchWord, page) {
+    const fetchedData = await fetch(
+      `https://pixabay.com/api/?q=${searchWord}&page=${page}&key=27675022-eae91b965f306fbe1611b8e88&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(r => {
+        if (r.ok) {
+          return r.json();
+        }
+        Promise.reject(new Error('error'));
+      })
+      .then(r =>
+        r.hits.map(el => {
+          const itemId = el.id;
+          const itemPicture = el.webformatURL;
+          const itemlargeImage = el.largeImageURL;
 
-    const page = 1;
-    const searchWord = e.currentTarget.elements.formInput.value;
-
-    if (searchWord) {
-      await this.setState({
-        page,
-        searchWord,
-        isLoading: true,
-      });
-      let items = await fetch(
-        `https://pixabay.com/api/?q=${searchWord}&page=${page}&key=27675022-eae91b965f306fbe1611b8e88&image_type=photo&orientation=horizontal&per_page=12`
+          return { itemId, itemPicture, itemlargeImage };
+        })
       )
-        .then(r => {
-          if (r.ok) {
-            return r.json();
-          }
+      .catch(error => console.log(error));
 
-          Promise.reject(new Error('error'));
-        })
-        .then(r => {
-          this.setState({ total: r.total });
-          return r.hits.map(el => {
-            const itemId = el.id;
-            const itemPicture = el.webformatURL;
-            const itemlargeImage = el.largeImageURL;
+    return fetchedData;
+  }
 
-            return { itemId, itemPicture, itemlargeImage };
-          });
-        })
-        .catch(error => console.log(error));
+  async componentDidUpdate(prevProps, prevState) {
+    const { searchWord, page } = this.state;
 
-      this.setState({
-        isLoading: false,
-        items,
-      });
-    }
-  };
-
-  loadMore = async e => {
-    const page = this.state.page + 1;
-
-    try {
-      await this.setState({
-        page,
-        isLoading: true,
-      });
-      const moreItems = await fetch(
-        `https://pixabay.com/api/?q=${this.state.searchWord}&page=${page}&key=27675022-eae91b965f306fbe1611b8e88&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(r => {
-          if (r.ok) {
-            return r.json();
-          }
-          Promise.reject(new Error('error'));
-        })
-        .then(r =>
-          r.hits.map(el => {
-            const itemId = el.id;
-            const itemPicture = el.webformatURL;
-            const itemlargeImage = el.largeImageURL;
-
-            return { itemId, itemPicture, itemlargeImage };
-          })
-        );
+    const moreItems = await this.fetchData(searchWord, page);
+    if (
+      prevState.searchWord === this.state.searchWord &&
+      prevState.page !== this.state.page
+    ) {
       this.setState(prevState => {
         return {
           items: [...prevState.items, ...moreItems],
           isLoading: false,
         };
       });
-    } catch (error) {
-      console.log(error);
+    } else if (
+      prevState.searchWord !== this.state.searchWord &&
+      this.state.page === 1
+    ) {
+      this.setState({ items: moreItems, isLoading: false });
     }
+  }
+  submitForm = async e => {
+    e.preventDefault();
+    const inputValue = e.currentTarget.elements.formInput.value;
+    if (inputValue) {
+      this.setState(prevState => {
+        if (prevState.searchWord !== inputValue) {
+          return {
+            page: 1,
+            searchWord: inputValue,
+          };
+        }
+      });
+    }
+  };
+
+  loadMore = async e => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
   };
 
   openModal = e => {
