ae1b36ab01bca832566cb9be55c76f8431542b51
Вадим Фатеев <fateevdanon@gmail.com>
2022-08-06 17:07:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,60 +17,55 @@ export class App extends Component {
     total: 0,
   };
 
-  async fetchData(searchWord, page) {
-    const fetchedData = await fetch(
-      `https://pixabay.com/api/?q=${searchWord}&page=${page}&key=27675022-eae91b965f306fbe1611b8e88&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(r => {
-        if (r.ok) {
-          return r.json();
-        }
-        Promise.reject(new Error('error'));
-      })
-      .then(r =>
-        r.hits.map(el => {
-          const itemId = el.id;
-          const itemPicture = el.webformatURL;
-          const itemlargeImage = el.largeImageURL;
-
-          return { itemId, itemPicture, itemlargeImage };
-        })
-      )
-      .catch(error => console.log(error));
-
-    return fetchedData;
-  }
-
   async componentDidUpdate(prevProps, prevState) {
-    const { searchWord, page } = this.state;
+    const { searchWord, page, isLoading } = this.state;
 
-    const moreItems = await this.fetchData(searchWord, page);
-    if (
-      prevState.searchWord === this.state.searchWord &&
-      prevState.page !== this.state.page
-    ) {
+    if (prevState.searchWord !== searchWord || prevState.page !== page) {
+      if (!isLoading) {
+        this.setState({ isLoading: true });
+      }
+
+      const moreItems = await fetch(
+        `https://pixabay.com/api/?q=${searchWord}&page=${page}&key=27675022-eae91b965f306fbe1611b8e88&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(r => {
+          if (r.ok) {
+            return r.json();
+          }
+          Promise.reject(new Error('error'));
+        })
+        .then(r => {
+          if (prevState.total !== r.total) {
+            this.setState({ total: r.total });
+          }
+          return r.hits.map(el => {
+            const itemId = el.id;
+            const itemPicture = el.webformatURL;
+            const itemlargeImage = el.largeImageURL;
+
+            return { itemId, itemPicture, itemlargeImage };
+          });
+        })
+        .catch(error => console.log(error));
       this.setState(prevState => {
         return {
           items: [...prevState.items, ...moreItems],
           isLoading: false,
         };
       });
-    } else if (
-      prevState.searchWord !== this.state.searchWord &&
-      this.state.page === 1
-    ) {
-      this.setState({ items: moreItems, isLoading: false });
     }
   }
   submitForm = async e => {
     e.preventDefault();
     const inputValue = e.currentTarget.elements.formInput.value;
+
     if (inputValue) {
       this.setState(prevState => {
         if (prevState.searchWord !== inputValue) {
           return {
             page: 1,
             searchWord: inputValue,
+            items: [],
           };
         }
       });
@@ -79,7 +74,9 @@ export class App extends Component {
 
   loadMore = async e => {
     this.setState(prevState => {
-      return { page: prevState.page + 1 };
+      return {
+        page: prevState.page + 1,
+      };
     });
   };
 
